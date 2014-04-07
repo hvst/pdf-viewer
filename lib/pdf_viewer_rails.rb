6a10efc2150aa97bf33cdee3cd8e278acbb93dbd
Paul Sorensen <paulnsorensen@gmail.com>
2014-04-07 17:43:26 -0500

    Remove Railtie, Add group: :all to initializer

--- lib/pdf_viewer_rails.rb
+++ lib/pdf_viewer_rails.rb
@@ -1,5 +1,4 @@
 require "pdf_viewer_rails/engine"
-require "pdf_viewer_rails/railtie"
 
 module PdfViewerRails
 end
--- lib/pdf_viewer_rails/engine.rb
+++ lib/pdf_viewer_rails/engine.rb
@@ -1,5 +1,13 @@
 module PdfViewerRails
   class Engine < ::Rails::Engine
     isolate_namespace PdfViewerRails
+
+    initializer "pdf assets initialization", group: :all do |app|
+      app.config.assets.precompile += %w(
+        pdf_viewer_rails/application.js.erb
+        pdf_viewer_rails/pdf.worker.js
+        pdf_viewer_rails/viewer.css
+      )
+    end
   end
 end
