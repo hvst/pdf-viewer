5421d9616637a7f3b949ab4e44a2e0d3bff8b34a
Paul Sorensen <paulnsorensen@gmail.com>
2014-04-08 11:46:42 -0500

    Use send_file and proper caching to serve worker.js

--- app/controllers/pdf_viewer_rails/pdf_worker_controller.rb
+++ app/controllers/pdf_viewer_rails/pdf_worker_controller.rb
@@ -4,6 +4,37 @@ module PdfViewerRails
   class PdfWorkerController < ApplicationController
 
     def show
+      set_headers
+      respond_to do |format|
+        format.js do
+          send_file(
+            worker_path,
+            type: 'text/javascript',
+            disposition: 'inline'
+          )
+        end
+      end
+    end
+
+    private
+
+    def worker_path
+      @path ||=
+        PdfViewerRails::Engine.root.join(
+          'vendor',
+          'assets',
+          'javascripts',
+          'worker.js'
+        )
+    end
+
+    def worker_last_modified
+      @last_modified ||= File.mtime(worker_path)
+    end
+
+    def set_headers
+      response.headers['Cache-Control'] = 'public'
+      response.headers['Last-Modified'] = worker_last_modified
     end
   end
 end
