b2f8eb7f31d0c6a3994fd6d942794350bf9154a9
Enrique Alcantara <enriquealc27@gmail.com>
2014-04-08 09:39:26 -0400

    created controller to serve pdf.worker.js outside the asset pipeline

---
+++ app/controllers/pdf_viewer_rails/pdf_worker_controller.rb
@@ -0,0 +1,9 @@
+require_dependency "pdf_viewer_rails/application_controller"
+
+module PdfViewerRails
+  class PdfWorkerController < ApplicationController
+
+    def show
+    end
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,4 @@
 PdfViewerRails::Engine.routes.draw do
   root :to => 'pdf_viewer#show'
+  get 'pdf.worker', to: 'pdf_worker#show', as: 'pdf_worker'
 end
